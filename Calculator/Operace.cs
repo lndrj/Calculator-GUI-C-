16c3a0e3c1356e8cf29434836244ec20d65abef1
Lukáš Ondřej <90964511+lndrj@users.noreply.github.com>
2023-09-13 19:02:03 +0200

    Upload souborů
    Funkční konzolová OOP kalkulačka, jednoduchá

---
+++ Calculator/Operace.cs
@@ -0,0 +1,26 @@
+namespace Calculator;
+
+public class Operace
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+    
+    public double Minus(double a, double b)
+    {
+        return a - b;
+    }
+    
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    
+    public double Divide(double a, double b)
+    {
+        if (b == 0) throw new ArgumentException("Dělit nulou nelze");
+        
+        return a / b;
+    }
+}
